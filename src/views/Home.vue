2b5ca8262fa3ed78fe944cf59f4dd112ae69b495
WALEEDSALAH-PC\Professional <waleedomega@hotmail.com>
2021-03-15 22:54:16 +0200

    main components

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,35 @@
 <template>
   <div class="home">
-    
-   
+    <navbar />
+    <div id="main-section">
+      <sidebar />
+      <movie-list />
+    </div>
   </div>
 </template>
 
 <script>
+import Sidebar from "../components/Sidebar";
 // @ is an alias to /src
-
-
+import Navbar from "../components/Navbar";
+import MovieList from "../components/MovieList";
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    Navbar,
+    Sidebar,
+    MovieList
   }
 };
 </script>
+<style lang="scss" scoped>
+.home {
+  display: flex;
+  flex-direction: column;
+
+  #main-section {
+    display: flex;
+    margin-top: 50px;
+  }
+}
+</style>
