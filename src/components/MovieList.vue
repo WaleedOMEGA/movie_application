7664073fc08374b48b2bc0d24c5458d3a2ea735a
WALEEDSALAH-PC\Professional <waleedomega@hotmail.com>
2021-03-16 14:53:33 +0200

    movie list

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -1,10 +1,29 @@
 <template>
   <div id="movie-container">
     Movie List
+    <Movie v-for="movie in movies" :key="movie.id" :movie="movie" />
   </div>
 </template>
 <script>
-export default {};
+import Movie from "./Movie";
+export default {
+  components: {
+    Movie
+  },
+  data() {
+    return {
+      movies: []
+    };
+  },
+  computed: {
+    // movies() {
+    //   return this.$store.getters.getMovies;
+    // }
+  },
+  created() {
+    this.movies = this.$store.getters.getMovies;
+  }
+};
 </script>
 <style lang="scss" scoped>
 #movie-container {
